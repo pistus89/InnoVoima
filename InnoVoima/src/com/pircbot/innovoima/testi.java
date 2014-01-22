d72dbce9c1153b7e80c40bc90e851e3817d969a1
Krister Holmström <krister.holmstrom@gmail.com>
2014-01-22 17:58:28 +0200

    added javacsv.jar library and started developing schedule reminder in testi.java

--- InnoVoima/src/com/pircbot/innovoima/testi.java
+++ InnoVoima/src/com/pircbot/innovoima/testi.java
@@ -6,13 +6,17 @@ package com.pircbot.innovoima;
 
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.Date;
 import java.util.GregorianCalendar;
 import java.util.HashMap;
 import java.util.Scanner;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+
 import org.joda.time.Period;
+
+import com.csvreader.CsvReader;
 
 /**
  *
@@ -20,14 +24,20 @@ import org.joda.time.Period;
  */
 public class testi {
     public static void main(String[] args) {
-    	Scanner lukija = new Scanner(System.in);
-    	String url = lukija.nextLine();
-    	
-    	if (url.contains("www")) {
-    		System.out.println("true");
-    	} else {
-    		System.out.println("false");
-    	}
+    	try {
+			CsvReader schedule = new CsvReader("example.csv");
+			schedule.readHeaders();
+			
+			while(schedule.readRecord()) {
+				
+			}
+			
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
     } 	
 } 
 
