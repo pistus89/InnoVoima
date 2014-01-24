c6a3101fa2b75748ffb4338ab9aedc1ad620324d
Krister Holmström <a1202471@myy.haaga-helia.fi>
2014-01-24 14:48:06 +0200

    added Lession.java

---
+++ InnoVoima/src/com/pircbot/innovoima/Lession.java
@@ -0,0 +1,158 @@
+package com.pircbot.innovoima;
+
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
+public class Lession {
+	private String name;
+	private String code;
+	private Calendar starts;
+	private Calendar ends;
+	private String studyField;
+	private String language;
+	private int ops;
+	private String teacher;
+	private String campus;
+	private String programme;
+	private String group;
+	private Calendar startDate;
+	private Calendar endDate;
+	private String rooms;
+	
+	public Lession (String name, String code, String studyField, String language, String teacher,
+			String campus, String programme, String group, String rooms, Calendar starts, 
+			Calendar ends, Calendar startDate, Calendar endDate, int ops) {
+		this.name = name;
+		this.code = code;
+		this.starts = starts;
+		this.ends = ends;
+		this.studyField = studyField;
+		this.language = language;
+		this.ops = ops;
+		this.teacher = teacher;
+		this.campus = campus;
+		this.programme = programme;
+		this.group = group;
+		this.startDate = startDate;
+		this.endDate = endDate;
+		this.rooms = rooms;
+	}
+	
+	public Lession () {
+		this("","","","","","","","","", new GregorianCalendar(), new GregorianCalendar(),
+				new GregorianCalendar(), new GregorianCalendar(), 0);
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getCode() {
+		return code;
+	}
+
+	public void setCode(String code) {
+		this.code = code;
+	}
+
+	public Calendar getStarts() {
+		return starts;
+	}
+
+	public void setStarts(Calendar starts) {
+		this.starts = starts;
+	}
+
+	public Calendar getEnds() {
+		return ends;
+	}
+
+	public void setEnds(Calendar ends) {
+		this.ends = ends;
+	}
+
+	public String getStudyField() {
+		return studyField;
+	}
+
+	public void setStudyField(String studyField) {
+		this.studyField = studyField;
+	}
+
+	public String getLanguage() {
+		return language;
+	}
+
+	public void setLanguage(String language) {
+		this.language = language;
+	}
+
+	public int getOps() {
+		return ops;
+	}
+
+	public void setOps(int ops) {
+		this.ops = ops;
+	}
+
+	public String getTeacher() {
+		return teacher;
+	}
+
+	public void setTeacher(String teacher) {
+		this.teacher = teacher;
+	}
+
+	public String getCampus() {
+		return campus;
+	}
+
+	public void setCampus(String campus) {
+		this.campus = campus;
+	}
+
+	public String getProgramme() {
+		return programme;
+	}
+
+	public void setProgramme(String programme) {
+		this.programme = programme;
+	}
+
+	public String getGroup() {
+		return group;
+	}
+
+	public void setGroup(String group) {
+		this.group = group;
+	}
+
+	public Calendar getStartDate() {
+		return startDate;
+	}
+
+	public void setStartDate(Calendar startDate) {
+		this.startDate = startDate;
+	}
+
+	public Calendar getEndDate() {
+		return endDate;
+	}
+
+	public void setEndDate(Calendar endDate) {
+		this.endDate = endDate;
+	}
+
+	public String getRooms() {
+		return rooms;
+	}
+
+	public void setRooms(String rooms) {
+		this.rooms = rooms;
+	}
+
+}
--- InnoVoima/src/com/pircbot/innovoima/testi.java
+++ InnoVoima/src/com/pircbot/innovoima/testi.java
@@ -7,6 +7,8 @@ package com.pircbot.innovoima;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.GregorianCalendar;
 import java.util.HashMap;
@@ -16,6 +18,7 @@ import java.util.logging.Logger;
 
 import org.joda.time.Period;
 
+import com.pircbot.*;
 import com.csvreader.CsvReader;
 
 /**
@@ -24,21 +27,53 @@ import com.csvreader.CsvReader;
  */
 public class testi {
     public static void main(String[] args) {
-    	try {
-			CsvReader schedule = new CsvReader("example.csv");
-			schedule.readHeaders();
-			
-			while(schedule.readRecord()) {
-				
-			}
-			
-		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+    	
+    	Lession java = new Lession("java", "ICT4TN002-4", "ICT Core", "FIN",
+    			"Erkki Esimerkki", "Pasila", "Tiko", "TN4PC", "4001", saveClock(8,00), 
+    			saveClock(11,45), saveDate(21,1,2014), saveDate(28,2,2014), 3);
+    	Lession ruotsi = new Lession("ruotsi", "SWE4TN003-6", "Language", "FIN", "Elli Esimerkki",
+    			"Pasila", "Tiko", "TN4PC", "4001", saveClock(12,00), saveClock(15,45), 
+    			saveDate(20,1,2014), saveDate(28,2,2014),3);
+    	ArrayList<Lession> lessions = new ArrayList<Lession>();
+    	lessions.add(java);
+    	lessions.add(ruotsi);
+    	
+    	HashMap<String,ArrayList<String>> users = new HashMap<String,ArrayList<String>>();
+    	
+//        	try { 
+//			CsvReader schedule = new CsvReader("example.csv");
+//			schedule.readHeaders();
+//			
+//			while(schedule.readRecord()) {
+//				
+//			}
+//			
+//		} catch (FileNotFoundException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		} catch (IOException e) {
+//			e.printStackTrace();
+//		}
+    	
+    	
+    	
+    
+    
     } 	
+    
+    public static Calendar saveClock(int hours, int minutes) {
+    	Calendar start = new GregorianCalendar();
+    	start.set(Calendar.HOUR_OF_DAY, hours);
+    	start.set(Calendar.MINUTE, minutes);
+    	
+    	return start;
+    }
+    
+    public static Calendar saveDate(int day, int month, int year) {
+    	Calendar start = new GregorianCalendar();
+    	start.set(year, month, day);
+    	return start;
+    }
 } 
 
 
